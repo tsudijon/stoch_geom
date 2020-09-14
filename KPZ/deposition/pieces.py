8418792317d2263d559f58ba2415a231819bf3b1
tsudijon <timothysudijono@gmail.com>
2020-09-13 20:58:57 -0400

    Added piece maker class and added functions to move board, check collision

--- KPZ/deposition/pieces.py
+++ KPZ/deposition/pieces.py
@@ -1,11 +1,39 @@
+import random
 
-class piece(object):
+class PieceMaker(object):
     def __init__(self):
-       pass 
+        return
+
+    def create_random_tetris_piece(self, top_left_coord):
+        pass
+
+    def create_simple_block(self, coord):
+        return Piece(set(coord))
+
+    def create_2b1_block(self, coord):
+        coords = set([coord, coord + (0,1)])
+        return Piece(coords)
+
+class Piece(object):
+    def __init__(self, coords):
+        self._indices = coords
+         
 
     def move_down(self):
-        pass
+        self._indices = self.get_indices('down')
+        return self._indices
+        
 
-    def get_indices(self):
-        pass
+    def get_indices(self, move = None):
+
+        step = (0,0)
+        if not move:
+            return self._indices
+        elif move == 'down':
+            step = (1,0)
+        else:
+            return self._indices
+            
+        moved_coords = [x + step for x in self._indices]
+        return set(moved_coords)
 ### initialize types of pieces here
--- KPZ/deposition/simulation.py
+++ KPZ/deposition/simulation.py
@@ -1,13 +1,16 @@
 import numpy as np
+from pieces import Piece, PieceMaker
+import random
 
 
-
-class board(object):
+class Board(object):
     def __init__(self, size = (1000,20)):
         self._size = size
         self._pieces = []
         self._board = np.zeros(size)
         self._used_indices = set() 
+
+        self._piece_factory = PieceMaker()
 
         #### set the bottom row to be used
         for i in range(self._size[1]):
@@ -15,27 +18,49 @@ class board(object):
         
         
     def update(self):
-        self._move_board()
-        self._initialize_pieces()
-        
-    def _move_board(self):
-        ### move all pieces downwards: update board and pieces?
+        self.move_board()
+        self.initialize_piece()
+        self.draw()
 
+        
+    def move_board(self):
+        ### move all pieces downwards: update board and pieces?
         ### update pieces if collision
         for piece in self._pieces:
-            if self._check_collision(piece):
-                self._used_indices.union(piece.get_indices())
+            if self.check_collision(piece):
+                self._used_indices = self._used_indices.union(piece.get_indices())
+                self._pieces.remove(piece)
             else:
                 piece.move_down()
 
         pass 
 
-    def _initialize_pieces(self):
+    def initialize_piece(self):
         ### add pieces with some probability
         ### check for collisions with new pieces?
-        pass
+        coord = (0,random.randrange(0, self._size[1]-1))
+        self._pieces.append(self._piece_factory.create_2b1_block(coord))
 
-    def _check_collision(self, piece):
+    def check_collision(self, piece):
+        ### also check for OOB but should be OK
+        if not self._check_in_bounds(piece):
+            return False
+
+        if self._used_indices.intersection(piece.get_indices('down')):
+            return False
+        else:
+            return True
+        ###
+
+    def _check_in_bounds(self, piece):
+        ### TODO: not implemented yet
+        return True    
+
+    #########################
+    ### drawing functions ###
+    #########################
+
+    def draw(self):
         pass
 
 
